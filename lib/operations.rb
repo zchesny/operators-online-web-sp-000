a7ea28b08620a06bae336e22c835cdcb4227743a
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 17:26:45 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,10 @@
 def unsafe?(speed)
-  speed > 60 || speed < 40 ? true : false
-  # if speed > 60: 
+  # speed > 60 || speed < 40 ? true : false
+  if speed > 60 || speed < 40:
+    return true
+  else
+    return false
+  # if speed > 60:
   #   return true
   # elsif speed < 40:
   #   return true
