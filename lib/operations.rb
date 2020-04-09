bfaa6cac5268ca6a3e0ae190bf6f4c0f28dce783
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 17:27:25 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,11 @@
 def unsafe?(speed)
-  speed > 60 || speed < 40 ? true : false
-  # if speed > 60: 
+  # speed > 60 || speed < 40 ? true : false
+  if speed > 60 || speed < 40
+    return true
+  else
+    return false
+  end
+  # if speed > 60:
   #   return true
   # elsif speed < 40:
   #   return true
